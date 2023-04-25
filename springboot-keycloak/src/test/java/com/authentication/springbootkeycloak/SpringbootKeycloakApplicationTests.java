d66cd68814dcdb4030bf3f7b17e83b92b292c3e5
knightch <amankumarpandey2397@gmail.com>
2023-04-25 18:50:49 +0530

    initial setup

---
+++ springboot-keycloak/src/test/java/com/authentication/springbootkeycloak/SpringbootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.springbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
